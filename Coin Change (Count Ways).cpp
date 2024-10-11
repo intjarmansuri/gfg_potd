4d5ced746b2ff571dbbe5f24857239a9c899ac0b
Intjar Mansuri <intjar1606@gmail.com>
2024-10-11 19:27:35 +0530

    DP

---
+++ Coin Change (Count Ways).cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    long long int count(int coins[], int N, int sum) {
+        
+        long long dp[sum+1] = {0};
+        
+        dp[0] = 1;
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = coins[i]; j <= sum; j++) {
+                
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[sum];
+    }
+};
