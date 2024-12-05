a4c1e498989fabd8beecad6f1817dd692d94daeb
Intjar Mansuri <intjar1606@gmail.com>
2024-12-05 14:36:38 +0530

    Again Solve Dec-2024

--- Sort 0s, 1s and 2s.cpp
+++ Sort 0s, 1s and 2s.cpp
@@ -26,3 +26,31 @@ class Solution {
         }
     }
 };
+
+
+/* ------------------------ Approach : Dutch National Flag Algorithm ------------------------*/
+// Time Complexity : O(n)
+// Space Compelxity : O(1)
+
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.size() - 1;
+        
+        while(mid <= high) {
+            if(arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } else if(arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
