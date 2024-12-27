9455f7e43fe591a7dff816eff6a65670a3f7397f
Intjar Mansuri <intjar1606@gmail.com>
2024-12-27 22:01:20 +0530

    Dec-2024

---
+++ Count pairs with given sum.cpp
@@ -0,0 +1,25 @@
+/* ------------------------ Approach : Hashmap -----------------------*/
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        
+        unordered_map<int, int> freq;
+        
+        int count = 0;
+        
+        for (int num : arr) {
+            int complement = target - num;
+            
+            if (freq.find(complement) != freq.end()) {
+                count += freq[complement];
+            }
+            
+            freq[num]++;
+        }
+        
+        return count;
+    }
+};
