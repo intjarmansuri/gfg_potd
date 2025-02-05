4de40f813e83b5f33efdcbfa5b6966c7141cd8e9
Intjar Mansuri <intjar1606@gmail.com>
2025-02-05 22:25:56 +0530

    DFS Recursive

--- Mirror tree.cpp
+++ Mirror tree.cpp
@@ -1,14 +1,16 @@
+/* ------------------------------ Approach : Recursive Solution (DFS) ------------------------------ */
+// Time Complexity : O(N)
+// Space Complexity : O(H)
+
+
 class Solution {
   public:
     void mirror(Node* node) {
+       if(node == NULL) return;
         
-        if(!node) return;
+        swap(node->left, node->right);
         
         mirror(node->left);
         mirror(node->right);
-        
-        Node* temp = node->left;
-        node->left = node->right;
-        node->right = temp;
     }
 };
